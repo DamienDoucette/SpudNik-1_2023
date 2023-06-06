f1b837ccc561bf8abd2f45cd0041a596995a2444
DamienDoucette <dldoucette@upei.ca>
2023-06-06 20:13:51 -0300

    Update main.cpp
    Added the function for the ACS thread and put an outline for the logic from the flowchart.
    
    Variable types and sizes will need to be fixed when I have a better understanding of what is going on.
    
    Got the thread to function properly in the main function

--- main.cpp
+++ main.cpp
@@ -18,19 +18,99 @@
 
 using namespace std;
 
+//Declare global variables
+bool startFlag = 1;
+bool pwrFlag = 0;
+bool comFlag = 0;
+bool imgFlag = 0;
+
+void acs(){
+    /*
+    This function will contain the thread for the ACS software for the Satellite
+
+    Sub-functions for this thread are stored in a seperate file under the 'threads' folder
+
+    The logic for this thread was based off the '0.Main.drawio' file in the 'ConOps' GDrive
+    */
+
+    //Delcare local vairables
+    double w = 0;
+    double posErr = 0;
+    double bField[3];
+    double w_RWA[2];
+    uint8_t ATT_MOD = 0x00; //Will have to check what defualt value is supposed to be
+
+    //Logic defined in FlowChart
+    printf("ACS Function started\n");
+    if(startFlag){
+        printf("Start flag true\n");
+        sensingAndEstimation();
+        if(pwrFlag){
+            if(w < 1){
+                if(posErr < 1){
+                    if(comFlag){
+                        ATT_MOD = 0x10;
+                        readTargetAttitudeCOMMS();
+
+                        positionControl();
+
+                        if(1){ //Target attitude met
+                            //Set attFlag
+                        } else {
+                            //Reset attFlag
+                        }
+                    } else {
+                        if(imgFlag){
+                            ATT_MOD = 0x11;
+                            readTargetAttitudeIMG();
+
+                            positionControl();
+
+                            if(1){ //Target attitude met
+                            //Set attFlag
+                            } else {
+                                //Reset attFlag
+                            }
+                        } else {
+                            ATT_MOD = 0x00;
+                            detumble();
+                        }
+                    }
+                } else {
+                    ATT_MOD = 0x01;
+                    calibration();
+                }
+
+            } else {
+                ATT_MOD = 0x00;
+                detumble(); //Might need arguements? one call uses them and one doesnt in flowchart
+            }
+        }
+    }
+}
+
 void setup(){
+    printf("Beginning setup loop...\n");
+
+    printf("Setup Complete.\n");
 
 }
 
 void loop(){
-    sensingAndEstimation();
+    
 }
 
 int main()
 {
     setup();
 
-    while(1){
-        loop();
-    }
+    thread acsThread(acs);
+    printf("ACS Thread started...\n");
+
+    acsThread.join();
+
+    // while(1){
+    //     loop();
+    // }
+
 }
