ac8d57747d13df842acd6a670c8e5d18db108118
DamienDoucette <dldoucette@upei.ca>
2023-06-06 20:15:08 -0300

    Update acs.cpp
    Added separate functions for readTargetAttitudeCOMMS and readTargetAttitudeIMG
    
    In addition added placeholders for the remaining functions/processes that are called in the ACS thread

--- threads/acs.cpp
+++ threads/acs.cpp
@@ -1,5 +1,36 @@
+/**
+ * @file acs.cpp
+ * @author Damien Doucette
+ * @brief This file contains all of the functions/processes that will be called under the ACS thread in 'main.cpp'
+ * @version 0.1
+ * @date 2023-06-06
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include "acs.h"
 
 void sensingAndEstimation(){
-    printf("Sensing and Estimation called!");
+    printf("Sensing and Estimation called!\n");
+}
+
+void detumble(){
+    printf("Detumbling called!\n");
+}
+
+void calibration(){
+    printf("Calibration called!\n");
+}
+
+void readTargetAttitudeCOMMS(){
+    printf("Read Target Attitiude (COMMS) called!\n");
+}
+
+void readTargetAttitudeIMG(){
+    printf("Read Target Attitiude (IMAGER) called!\n");
+}
+
+void positionControl(){
+    printf("Position Control called!\n");
 }
