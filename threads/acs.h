4eb7e68aa6f9f933dd1060150cb941cb53778de0
DamienDoucette <dldoucette@upei.ca>
2023-06-07 12:56:44 -0300

    Update acs.h
    Added arguements to sensingAndEstimation based off the flowchart of the function

--- threads/acs.h
+++ threads/acs.h
@@ -12,7 +12,7 @@ extern bool imgFlag;
 //Define functions required under the ACS thread
 /*NOTE: Output types will need to be fixed at a later time - output type is not known at this time*/
 
-void sensingAndEstimation();
+void sensingAndEstimation(char TLE[2][69], float tprev);
 void detumble();
 void calibration();
 void readTargetAttitudeCOMMS();
