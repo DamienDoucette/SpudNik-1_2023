020769b21ca9755e9b3c2848395c86eae9ddaf92
DamienDoucette <dldoucette@upei.ca>
2023-06-07 12:57:46 -0300

    Create powerManagement.cpp
    Created a .cpp file for the function contained under the powerManagement thread
    
    Started laying out the checkBattery function

---
+++ threads/powerManagement.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file powerManagement.cpp
+ * @author Damien Doucette (dldoucette@upei.ca)
+ * @brief This document holds the subfunctions of the Power Management thread
+ * @version 0.1
+ * @date 2023-06-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "powerManagement.h"
+
+float checkBattery(){
+    /* This function is used to check the battery level from the power management IC*/
+
+    float batteryLevel = NULL;
+
+    /*UART Communication for the IC*/
+
+    return batteryLevel;
+}
