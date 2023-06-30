10e3267458d0d429dcfadba13d69c00c794c997f
DamienDoucette <dldoucette@upei.ca>
2023-06-30 15:07:47 -0300

    Update i2c-lib
    Changed the library so that the read function would take a pointer as a parameter and store the data there, as opposed to returning a pointer to a memory address.

--- hardwareInterfacing/i2c-lib/i2c.cpp
+++ hardwareInterfacing/i2c-lib/i2c.cpp
@@ -1,7 +1,13 @@
 #include "i2c.h"
 
 i2c::i2c(const char* i2cBus){
-    char *filename = (char*)i2cBus;
+    /* Constructor
+        Parameter:
+            i2cBus  -   Name of the file used to read and write to the I2C bus. 
+                        This parameter has a default value of "/dev/i2c-1"
+    */
+
+    char *filename = (char*)i2cBus; //Declare pointer to the I2C bus file
 
     /*Open I2C Bus*/
     if((file_i2c = open(filename, O_RDWR)) < 0){
@@ -9,33 +15,63 @@ i2c::i2c(const char* i2cBus){
     }
 }
 
-uint8_t* i2c::readBus(uint8_t address, int length){
-    if(ioctl(file_i2c, I2C_SLAVE, address) < 0){
-        printf("ERROR:\t Failed to contact client");
-    }
-    if(read(file_i2c, buffer, length) != length){
-        printf("ERROR:\tFailed to read bytes from client\n");
-    } else {
-        
-    }
-    return buffer;
-}
+int i2c::readBus(uint8_t address, int length, uint8_t *buffer){
+    /*
+        readBus(uint8_t address, int length, uint8_t buffer[n]) - This method is used to read from a client on the I2C bus.
+            Parameters:     
+                address     -   7bit address of the client to read data from
+                length      -   Length of the transmission in bytes
+                buffer      -   Pointer to the memory address to write readings to
 
-int i2c::writeBus(uint8_t address, int length, uint8_t writeBuffer[3]){
-
+            Return:
+                0           -   The transmission was successful
+                ~0          -   An error occured
+    */
+   //Contact the client
     if(ioctl(file_i2c, I2C_SLAVE, address) < 0){
         printf("ERROR:\t Failed to contact client");
         return 2;
     }
 
+    //Read data from the client
+    if(read(file_i2c, buffer, length) != length){
+        printf("ERROR:\tFailed to read bytes from client\n");
+        return 3;
+    }
+    return 0;
+}
+
+int i2c::writeBus(uint8_t address, int length, uint8_t writeBuffer[3]){
+    /*
+    writeBus(uint8_t address, int length, uint8_t buffer[n]) - This method is to write to a client on the I2C bus
+            Parameters:
+                address     -   7bit address of the client to read data from
+                length      -   Length of the transmission in bytes
+                buffer      -   array containing the bytes to be written to the bus. Index 0 of the array will be the first byte transmitted
+
+            Return:
+                0           -   The transmission was successful
+                ~0          -   An error occured
+    */
+
+    //Contact the client on the I2C bus
+    if(ioctl(file_i2c, I2C_SLAVE, address) < 0){
+        printf("ERROR:\t Failed to contact client");
+        return 2;
+    }
+
+    //Write the bytes from the buffer to the device on the bus
     if (write(file_i2c, writeBuffer, length) != length)
 	{
 		printf("ERROR:\tFailed to write to slave.\n");
         return 4;
 	}
+    
     return 0;
 }
 
 void i2c::closeBus(){
+    /*closeBus() - This method is to close the I2C bus
+    */
     close(file_i2c);
 }
--- hardwareInterfacing/i2c-lib/i2c.h
+++ hardwareInterfacing/i2c-lib/i2c.h
@@ -10,19 +10,49 @@
 /*Other libraries*/
 #include <cstdint>
 #include <stdio.h>
-#include <cstring>
 
 class i2c {
+    /*  This class is to provide a simple I2C interface to allow data to be 
+        written to and read from the bus.
+
+        The constructor takes a single argument with is the I2C bus file location
+        This is set to a default value of "/dev/i2c-1" which should be correct for the RPI
+
+        The class has three methods:
+
+        readBus(uint8_t address, int length, uint8_t buffer[n]) - This method is used to read from a client on the I2C bus.
+            Parameters:     
+                address     -   7bit address of the client to read data from
+                length      -   Length of the transmission in bytes
+                buffer      -   Pointer to the memory address to write readings to
+
+            Return:
+                0           -   The transmission was successful
+                ~0          -   An error occured
+
+
+        writeBus(uint8_t address, int length, uint8_t buffer[n]) - This method is to write to a client on the I2C bus
+            Parameters:
+                address     -   7bit address of the client to read data from
+                length      -   Length of the transmission in bytes
+                buffer      -   array containing the bytes to be written to the bus. Index 0 of the array will be the first byte transmitted
+
+            Return:
+                0           -   The transmission was successful
+                ~0          -   An error occured
+
+        closeBus() - This method is to close the I2C bus
+     */
     private:
-        int addr;
-        int file_i2c;
-        char *filename;
-        uint8_t buffer[60];
+        /* Declare private variables */
+        int file_i2c;       //Variable to read and write to the I2C file
+        char *filename;     //Pointer to the I2C bus file
 
     public:
-        i2c(const char* i2cBus);
-        uint8_t *readBus(uint8_t address, int length);
-        int writeBus(uint8_t address, int length, uint8_t buffer[3]);
+    /* Delcare methods */
+        i2c(const char* i2cBus = "/dev/i2c-1");
+        int readBus(uint8_t address, int length, uint8_t *buffer);
+        int writeBus(uint8_t address, int length, uint8_t *buffer);
         void closeBus();
 
 };
