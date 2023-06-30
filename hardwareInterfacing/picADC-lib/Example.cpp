eae68204e1488e9f146035226641df3a00e86a12
DamienDoucette <dldoucette@upei.ca>
2023-06-30 15:08:35 -0300

    Create Example.cpp
    Created an example script to show how to use the picADC library

---
+++ hardwareInterfacing/picADC-lib/Example.cpp
@@ -0,0 +1,27 @@
+#include "../i2c-lib/i2c.h"   //Include I2C library
+#include "picADC.h"    //Include ADC library
+
+#include <stdio.h>
+
+int main(){
+    i2c I2C;  //Create instance of the I2C class
+	int addr = 0x68;    //Define address of client
+
+    picADC adc(addr, I2C);  //Create instance of picADC class
+
+    /* READ ADC VALUES */
+    uint16_t readings[12];  //Define array to hold the readings values
+    adc.readSunSensors(readings);   //Call the function to read the sensors and pass the output array
+
+    /* PRINT VALUES */
+    printf("        Sun Sensor Readings         \n");
+    printf("====================================\n");
+
+    for(int i = 0; i < 12; i++){
+        printf("Reading %d:\t%d\n", i+1, readings[i]);
+    }
+
+    printf("====================================\n");
+    
+    return 0;
+}
