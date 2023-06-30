e9d10b88642e93c792c04815b15df9d41ab484d9
DamienDoucette <dldoucette@upei.ca>
2023-06-30 15:15:30 -0300

    Updated picPWM-lib
    Changed methods to void instead of int

--- hardwareInterfacing/picPWM-lib/picPWM.cpp
+++ hardwareInterfacing/picPWM-lib/picPWM.cpp
@@ -4,9 +4,9 @@ void writeToI2C(i2c& I2C, uint8_t addrMCU, uint16_t addrReg, uint8_t dutyCycle){
     
     /*Organize the data into bytes for the I2C write buffer*/
     uint8_t writeBuffer[3];
-    writeBuffer[0] = addressReg >> 8;  //Address H
-    writeBuffer[1] = addressReg & ~(0b11110000);   //Address L
-    writeBuffer[2] = duty_cycle;
+    writeBuffer[0] = addrReg >> 8;  //Address H
+    writeBuffer[1] = addrReg & ~(0xFF00);   //Address L
+    writeBuffer[2] = dutyCycle;
 
     /*Use I2C library to send data to the MCU*/
     if(I2C.writeBus(addrMCU, 3, writeBuffer)){
@@ -19,32 +19,56 @@ picPWM::picPWM(uint8_t address, i2c& I2Cobj){
     addr = address;
 }
 
-int picPWM::setMagX(uint8_t duty_cycle){
+void picPWM::setMagX(uint8_t duty_cycle){
+    /*
+    setMagX:    Method to set the PWM signal for the magnetorquer on the X axis
+            Parameters:
+                duty_cycle  -   duty cycle value ranging from 0-199
+          
+    */
     uint16_t address= 0x46B;   //Memory address for PWM1S1P1 duty cycle
     writeToI2C(I2C, addr, address, duty_cycle);     //Write data to MCU
-    return 0;
 }
 
-int picPWM::setMagY(uint8_t duty_cycle){
+void picPWM::setMagY(uint8_t duty_cycle){
+    /*
+    setMagY:    Method to set the PWM signal for the magnetorquer on the Y axis
+            Parameters:
+                duty_cycle  -   duty cycle value ranging from 0-199
+
+    */
     uint16_t address = 0x46D;   //Memory address for PWM1S1P2 duty cycle
     writeToI2C(I2C, addr, address, duty_cycle); //Write data to MCU
-    return 0;
 }
 
-int picPWM::setMagZ(uint8_t duty_cycle){
+void picPWM::setMagZ(uint8_t duty_cycle){
+    /*
+    setMagZ:    Method to set the PWM signal for the magnetorquer on the Z axis
+            Parameters:
+                duty_cycle  -   duty cycle value ranging from 0-199
+
+    */
     uint16_t address = 0x47A;   //Memory address for PWM2S1P1 duty cycle
     writeToI2C(I2C, addr, address, duty_cycle); //Write data to MCU
-    return 0;
 }
 
-int picPWM::setMotX(uint8_t duty_cycle){
+void picPWM::setMotX(uint8_t duty_cycle){
+    /*
+    setMotX:    Method to set the PWM signal for the reaction wheel on the X axis
+            Parameters:
+                duty_cycle  -   duty cycle value ranging from 0-199
+
+    */
     uint16_t address = 0x489;   //Memory address for PWM3S1P1 duty cycle
     writeToI2C(I2C, addr, address, duty_cycle); //Write data to MCU
-    return 0;
 }
 
-int picPWM::setMotY(uint8_t duty_cycle){
+void picPWM::setMotY(uint8_t duty_cycle){
+    /*
+    setMotY:    Method to set the PWM signal for the reaction wheel on the Y axis
+            Parameters:
+                duty_cycle  -   duty cycle value ranging from 0-199
+    */
     uint16_t address = 0x48B;   //Memory address for PWM3S1P2 duty cycle
     writeToI2C(I2C, addr, address, duty_cycle); //Write data to MCU
-    return 0;
 }
--- hardwareInterfacing/picPWM-lib/picPWM.h
+++ hardwareInterfacing/picPWM-lib/picPWM.h
@@ -1,23 +1,71 @@
 #ifndef PICPWM_H
 #define PICPWM_H
 
+/*Include libraries*/
 #include <stdint.h>
 #include "../i2c-lib/i2c.h"
 
 class picPWM {
+    /* Class to control the PWM signals for the magnetorquers and reaction wheel motors
+        This is specifically designed to interface with the PIC18 MicroChip MCUs
+
+        Note that the various PWM devices will need to be connected to the proper pins on the MCU
+        to ensure proper functionality of this software. This software uses the memory addresses 
+        of the registers to control each PWM output.  If a different PWM module output is used for a
+        given acutator, the address values stored in the various methods will need to be updated to reflect
+        this change. It is recommended to leave this as is unless nessecary.
+        
+        The pin requirements are as follows:
+        PIN     |   Actuator        |   PWM output
+        =============================================
+        RC4     |   Motor X         |   PWM3S1P1
+        RC3     |   Motor Y         |   PWM3S1P2
+        RC6     |   Magnetorquer X  |   PWM1S1P1
+        RC7     |   Magnetorquer Y  |   PWM1S1P2
+        RB7     |   Magnetorquer Z  |   PWM2S1P1
+
+        Constructor picPWM(uint8_t address, i2c& I2Cobj):
+            Parameters:
+                address -   7bit I2C address of the PIC18 MCU
+                I2Cobj  -   Reference to the instance of the i2c class
+
+        Methods:
+            setMagX:    Method to set the PWM signal for the magnetorquer on the X axis
+                Parameters:
+                    duty_cycle  -   duty cycle value ranging from 0-199
+                    
+            setMagY:    Method to set the PWM signal for the magnetorquer on the Y axis
+                Parameters:
+                    duty_cycle  -   duty cycle value ranging from 0-199
+
+            setMagZ:    Method to set the PWM signal for the magnetorquer on the Z axis
+                Parameters:
+                    duty_cycle  -   duty cycle value ranging from 0-199
+
+            setMotX:    Method to set the PWM signal for the reaction wheel on the X axis
+                Parameters:
+                    duty_cycle  -   duty cycle value ranging from 0-199
+
+            setMotY:    Method to set the PWM signal for the reaction wheel on the Y axis
+                Parameters:
+                    duty_cycle  -   duty cycle value ranging from 0-199
+    */
+   
     private:
+        //Define class variables
         uint8_t addr;
-        i2c& I2C;
+        i2c I2C;
 
     public:
+        //Define methods
         picPWM(uint8_t address, i2c& I2Cobj);
 
-        int setMagX(uint8_t duty_cycle);
-        int setMagY(uint8_t duty_cycle);
-        int setMagZ(uint8_t duty_cycle);
+        void setMagX(uint8_t duty_cycle);
+        void setMagY(uint8_t duty_cycle);
+        void setMagZ(uint8_t duty_cycle);
 
-        int setMotX(uint8_t duty_cycle);
-        int setMotY(uint8_t duty_cycle);
+        void setMotX(uint8_t duty_cycle);
+        void setMotY(uint8_t duty_cycle);
 };
 
 #endif
