b0c8aa8ac177dce1d68bdd275a8bf156e6116252
DamienDoucette <dldoucette@upei.ca>
2023-06-30 15:11:26 -0300

    Update picADC-lib
    Changed the readSenSensors function to take a pointer to an address as an argument and then save the readings there as opposed to returning a pointer.

--- hardwareInterfacing/picADC-lib/picADC.h
+++ hardwareInterfacing/picADC-lib/picADC.h
@@ -5,13 +5,30 @@
 #include "../i2c-lib/i2c.h"
 
 class picADC{
+    /*
+        Class to read the ADC values from the sun sensors
+
+        Constructor picADC(uint8_t address, i2c& I2Cobj)
+            Parameters:
+                address -   7bit I2C address of the PIC18 MCU
+                I2Cobj  -   Reference to instance of i2c class 
+
+        Methods:
+            readSunSensors(uint16_t* readings)    -   Reads the 12 sun sensor values and stores values to 'readings'
+                Parameters:
+                    readings    -   Pointer to the location that the ADC readings will be saved
+
+    */
+
     private:
+        //Declare class variables
         uint8_t addr;
-        i2c& I2C;
+        i2c I2C;
 
     public:
+        //Declare methods
         picADC(uint8_t address, i2c& I2Cobj);
 
-        uint16_t* readSunSensors(uint8_t address);
-}
+        void readSunSensors(uint16_t* readings);
+};
 #endif
